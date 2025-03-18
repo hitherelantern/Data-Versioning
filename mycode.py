7f40ddf2c8fe05e428cf3627f232bbc5945f0367
lantern <lantern153pdt@gmail.com>
2025-03-18 15:50:21 +0530

    6-data v2

--- mycode.py
+++ mycode.py
@@ -10,8 +10,8 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
 df = pd.DataFrame(data)
 
 # # Adding new row to df for V2
-# new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
-# df.loc[len(df.index)] = new_row_loc
+new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
 
 # # Adding new row to df for V3
 # new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
@@ -28,3 +28,4 @@ file_path = os.path.join(data_dir, 'sample_data.csv')
 df.to_csv(file_path, index=False)
 
 print(f"CSV file saved to {file_path}")
+
